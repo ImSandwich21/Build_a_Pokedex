9dbf360e1f7605cad775cd88c6fc3a79f6237dce
ImSandwich21 <daniel-21-silva@hotmail.com>
2025-09-20 20:22:25 +0100

    adding pokeapi

--- main.go
+++ main.go
@@ -1,5 +1,16 @@
 package main
 
+import (
+	"time"
+
+	"github.com/ImSandwich21/Build_a_Pokedex/internal/pokeapi"
+)
+
 func main() {
-	startRepl()
+	pokeClient := pokeapi.NewClient(5 * time.Second)
+	cfg := &config{
+		pokeapiClient: pokeClient,
+	}
+
+	startRepl(cfg)
 }
